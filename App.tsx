d6649c9094ebd05cef1fd551d7cccc0939af361e
toantammute <21110797@student.hcmute.edu.vn>
2024-11-15 21:17:48 +0700

    get pets by user, reload

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
 import Navigation from './components/Navigation';
 import { AuthProvider } from './context/AuthContext';
-import { PetProvider } from './context/PetContext';
 import { StatusBar } from 'react-native';
 
 
 const App = () => {
   return (
     <AuthProvider>
-      <PetProvider>
-        <StatusBar backgroundColor='#06bcee'/>
-        <Navigation />
-      </PetProvider>
+      <StatusBar backgroundColor='#06bcee'/>
+      <Navigation />
     </AuthProvider>
   );
 };
