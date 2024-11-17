89a404f2338d2df4f14c883bc57440b3cd60fcf0
toantammute <21110797@student.hcmute.edu.vn>
2024-11-17 13:49:30 +0700

    form create

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,29 +9,49 @@ import RegisterScreen from '../src/RegisterScreen';
 import { AuthContext } from '../context/AuthContext';
 import SplashScreen from '../src/SplashScreen';
 import TabLayout from '../src/(tabs)/_layout';
+import ReminderStack from './reminder/ReminderStack';
+import NotificationScreen from '../src/NotificationScreen';
+import { RootStackParamList } from '../types';
+
+
+// type RootStackParamList = {
+//   Reminders: undefined;
+//   CreateReminder: undefined;
+//   Splash: undefined;
+//   Tab: undefined;
+//   Login: undefined;
+//   Register: undefined;
+//   // Add other screens here if needed
+// };
 
 const Stack = createNativeStackNavigator();
 
 
 const Navigation = () => {
 
-  const {userInfo, splashLoading} = useContext(AuthContext);
-  
+  const { userInfo, splashLoading } = useContext(AuthContext);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {splashLoading ? 
-        (
-          <Stack.Screen name='Splash' component={SplashScreen} options={{headerShown:false}}/>
-        ) : 
-        userInfo.access_token ? (
-          <Stack.Screen name='Tab' component={TabLayout} options={{headerShown:false}}/>
-        ) : (
+        {splashLoading ?
+          (
             <>
-            <Stack.Screen name='Login' component={LoginScreen} options={{headerShown:false}}/>
-            <Stack.Screen name='Register' component={RegisterScreen} options={{headerShown:false}}/>
+              <Stack.Screen name='Splash' component={SplashScreen} options={{ headerShown: false }} />
             </>
-        )}
+          ) :
+          userInfo.access_token ? (
+            <>
+              <Stack.Screen name='Tab' component={TabLayout} options={{ headerShown: false }} />
+              <Stack.Screen name="Notification" component={NotificationScreen} />
+              <Stack.Screen name='Reminders' component={ReminderStack} options={{ headerShown: false }}/>
+            </>
+          ) : (
+            <>
+              <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }} />
+              <Stack.Screen name='Register' component={RegisterScreen} options={{ headerShown: false }} />
+            </>
+          )}
       </Stack.Navigator>
     </NavigationContainer>
   );
