dcf20496ae971e97ecc45cba6259ba76945452c0
toantammute <21110797@student.hcmute.edu.vn>
2024-11-26 00:04:46 +0700

    disease detail

--- components/disease/DiseaseCard.tsx
+++ components/disease/DiseaseCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign'
+import { useNavigation } from '@react-navigation/native';
 
 interface Disease{
   disease_id: string;
@@ -13,8 +14,13 @@ interface DiseaseCardProps{
 }
 
 const DiseaseCard:React.FC<DiseaseCardProps> = ({disease}) => {
+  const navigation = useNavigation<any>();
+  const handlePress = () => {
+    console.log(disease.disease_id);
+    navigation.navigate('DiseaseDetailScreen', {disease_id: disease.disease_id});
+  }
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
       <Text style={styles.diseaseName}>{disease.disease_name}</Text>
       <AntDesign name="right" size={20} color="#666" />
     </TouchableOpacity>
--- hooks/useDisease.ts
+++ hooks/useDisease.ts
@@ -4,18 +4,45 @@ import axios from "axios";
 import { AuthContext } from '../context/AuthContext';
 
 interface Disease {
-    disease_id: string,
-    disease_name: string,
-    description: string,
+    disease_id: number;
+    disease_name: string;
+    description: string;
+    symptoms: string[];
+    treatment_phases: TreatmentPhase[];
+}
+
+interface TreatmentPhase {
+    phase_id: number;
+    phase_number: number;
+    phase_name: string;
+    duration: string;
+    phase_description: string;
+    phase_notes: string;
+    medicines: Medicine[];
+}
+
+interface Medicine {
+    medicine_id: number;
+    medicine_name: string;
+    usage: string;
+    dosage: string;
+    frequency: string;
+    duration: string;
+    side_effects: string;
 }
 
 const useDisease = () => {
     const [diseases, setDiseases] = React.useState<Disease[]>([]);
     const [diseaseLoading, setDiseaseLoading] = React.useState(false);
     const {userInfo} = useContext(AuthContext);
+    const [disease, setDisease] = React.useState<Disease | null>(null);
+    const [treatmentPhases, setTreatmentPhases] = React.useState<TreatmentPhase[]>([]);
+    const [medicines, setMedicines] = React.useState<Medicine[]>([]);
+    const [treatmentPhase, setTreatmentPhase] = React.useState<TreatmentPhase | null>(null);
+    const [medicine, setMedicine] = React.useState<Medicine | null>(null);
 
     const getDiseases = async () => {
-        console.log(`${API_URL}/dicease/`)
+        console.log(`${API_URL}/disease/`)
         setDiseaseLoading(true);
         try {
             const response = await axios.get(`${API_URL}/disease/`,{
@@ -33,7 +60,29 @@ const useDisease = () => {
             setDiseaseLoading(false);
         }
     };
+    const getTreatmentbyDisease = async (disease_id: string) => {
+        console.log(`${API_URL}/disease/treatment/${disease_id}`)
+        setDiseaseLoading(true);
+        try {
+            const response = await axios.get(`${API_URL}/disease/treatment/${disease_id}`,{
+                headers: {
+                    Authorization: `Bearer ${userInfo.access_token}`,
+                }
+            });
+            console.log('Diseases:', response.data.data);
+            // const diseaseData = response.data.data;
+            // setDisease(diseaseData);
+            // setTreatmentPhases(diseaseData.treatment_phases);
+            // setMedicines(diseaseData.treatment_phases[0].medicines);
+            setDiseaseLoading(false);
+        } catch (error) {
+            console.log('Error fetching diseases:', error);
+            setDiseaseLoading(false);
+        } finally {
+            setDiseaseLoading(false);
+        }
+    }
 
-    return { diseases, diseaseLoading, getDiseases };
+    return { diseases, diseaseLoading, getDiseases,getTreatmentbyDisease };
 }
 export default useDisease;
